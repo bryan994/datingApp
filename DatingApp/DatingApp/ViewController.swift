c88e1b6e52664ab922aa8f68cd37912e419e4828
Bryan <bryan994@outlook.com>
2016-10-20 10:12:57 +0800

    First Commit

---
+++ DatingApp/DatingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DatingApp
+//
+//  Created by Bryan Lee on 20/10/2016.
+//  Copyright © 2016 Bryan Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
